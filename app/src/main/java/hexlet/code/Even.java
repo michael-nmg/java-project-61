65721cb16fd6803695e901982a5fbbf2bfaf6dba
michael-nmg <michael.nmg@gmail.com>
2023-07-20 01:22:26 +0300

    fix Even class

--- app/src/main/java/hexlet/code/Even.java
+++ app/src/main/java/hexlet/code/Even.java
@@ -4,30 +4,25 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Even {
+    private static Random random = new Random();
 
     public static String game(String name) {
-        Random rand = new Random();
-        int count = 0;
         greeting();
+        int count = 0;
 
         while (count < 3) {
-            int number = rand.nextInt(1000);
+            int number = random.nextInt(1000);
             String check = (number % 2 == 0) ? "yes" : "no";
             String answer = doAnswer(number);
 
-            if (answer.equals(check)) {
-                count += 1;
-                System.out.println("Correct!");
-            } else {
-                return String.format("'%s' is wrong answer ;(. "
-                        + "Correct answer was '%s'.\n"
-                        + "Let's try again, %s!",
-                        answer,
-                        check,
-                        name);
+            if (!answer.equals(check)) {
+                return incorrect(answer, check, name);
             }
+
+            System.out.println("Correct!");
+            ++count;
         }
-        return String.format("Congratulations, %s!", name);
+        return itsTrue(name);
     }
 
     private static void greeting() {
@@ -35,8 +30,23 @@ public class Even {
     }
 
     private static String doAnswer(int number) {
-        System.out.format("Question: %s\n", number);
+        System.out.format("Question: %s%n", number);
         System.out.print("Your answer (yes or no): ");
         return new Scanner(System.in).nextLine();
+    }
+
+    private static String incorrect(String answer, String check, String name) {
+        return new StringBuilder("'")
+                .append(answer)
+                .append("' is wrong answer ;(. Correct answer was '")
+                .append(check)
+                .append("'.\nLet's try again, ")
+                .append(name)
+                .append("!\n")
+                .toString();
+    }
+
+    private static String itsTrue(String name) {
+        return String.format("Congratulations, %s!%n", name);
     }
 }
