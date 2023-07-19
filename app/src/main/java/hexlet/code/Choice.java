8ae668d34d7ce39d1febc59eeebce88e6e6c73f8
michael-nmg <michael.nmg@gmail.com>
2023-07-20 01:22:47 +0300

    fix Choice class

--- app/src/main/java/hexlet/code/Choice.java
+++ app/src/main/java/hexlet/code/Choice.java
@@ -5,46 +5,52 @@ import java.util.Scanner;
 public class Choice {
     private static String[] options = {"Greet", "Even"};
 
-    public static void choiceTitle() {
-        int size = options.length;
-        System.out.println("Please enter the game number and press Enter.");
+    public static void start() {
+        while (true) {
+            System.out.println(previewTitle());
+            Scanner scan = doSelect();
 
-        for (int i = 0; i < size; ++i) {
-            System.out.format("%s - %s\n", i + 1, options[i]);
-        }
+            if (scan.hasNextInt()) {
+                int choice = scan.nextInt();
 
-        System.out.println("0 - Exit");
-    }
-
-    public static void selectChoice() {
-        System.out.print("Your choice: ");
-        Scanner scan = new Scanner(System.in);
-
-        if (scan.hasNextInt()) {
-            int choice = scan.nextInt();
-
-            if (choice == 0) {
-                System.out.println("\nGoodbye.");
-            } else if (0 < choice && choice <= options.length) {
-                String name = Cli.greeting();
-                switch (choice) {
-                    case 2:
-                        System.out.println(Even.game(name));
-                        break;
-                    default:
-                        break;
+                if (choice < 0 || choice > options.length) {
+                    continue;
                 }
-            } else {
-                tryAgain();
+
+                System.out.print(selectChoice(choice));
+                break;
             }
-        } else {
-            tryAgain();
+
+            System.out.println("\nTry again.\n");
         }
     }
 
-    private static void tryAgain() {
-        System.out.println("\nTry again!\n");
-        choiceTitle();
-        selectChoice();
+    public static String previewTitle() {
+        StringBuilder list = new StringBuilder();
+        list.append("Please enter the game number and press Enter.\n");
+
+        for (int i = 0; i < options.length; ++i) {
+            list.append(String.format("%s - %s%n", i + 1, options[i]));
+        }
+
+        list.append("0 - Exit");
+        return list.toString();
+    }
+
+    public static String selectChoice(int choice) {
+        if (0 < choice) {
+            String name = Cli.greeting();
+            return switch (choice) {
+                case 2 -> Even.game(name);
+                case 3 -> "3";
+                default -> "";
+            };
+        }
+        return "\nGoodbye.\n";
+    }
+
+    private static Scanner doSelect() {
+        System.out.print("Your choice: ");
+        return new Scanner(System.in);
     }
 }
