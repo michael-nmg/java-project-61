965679bb82edcf6eb16bf0b506fb11330b27e868
michael-nmg <michael.nmg@gmail.com>
2023-07-22 02:54:26 +0300

    add new Gcd class

---
+++ app/src/main/java/hexlet/code/games/extn/Gcd.java
@@ -0,0 +1,29 @@
+package hexlet.code.games.extn;
+
+import hexlet.code.games.PatternForGame;
+
+public class Gcd extends PatternForGame {
+    @Override
+    protected String[] invariant() {
+        int left = random.nextInt(100);
+        int rigth = random.nextInt(100);
+        String check = String.valueOf(gcd(left, rigth));
+        String eval = String.format("%s %s", left, rigth);
+        String answer = doAnswer(eval);
+        return new String[]{answer, check};
+    }
+
+    @Override
+    protected void greeting() {
+        System.out.println("\nFind the greatest common divisor of given numbers.");
+    }
+
+    public String getName() {
+        return "GCD";
+    }
+
+    private int gcd(int a, int b) {
+        return (a % b == 0) ? Math.abs(b) : gcd(b, a % b);
+    }
+
+}
