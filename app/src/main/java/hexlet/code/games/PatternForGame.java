84eaa8a2f1bc520aa91effc5ab14ecb5083f9ae5
michael-nmg <michael.nmg@gmail.com>
2023-07-21 05:17:29 +0300

    add PatternForGame abstract class

---
+++ app/src/main/java/hexlet/code/games/PatternForGame.java
@@ -0,0 +1,53 @@
+package hexlet.code.games;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public abstract class PatternForGame implements Game {
+
+    protected final Random random = new Random();
+
+    public String start(String name) {
+        greeting();
+        int count = 0;
+
+        while (count < 3) {
+            String[] result = invariant();
+            String answer = result[0];
+            String check = result[1];
+
+            if (!answer.equals(check)) {
+                return incorrect(answer, check, name);
+            }
+
+            System.out.println("Correct!");
+            ++count;
+        }
+        return itsTrue(name);
+    }
+
+    protected abstract String[] invariant();
+
+    protected abstract void greeting();
+
+    protected String doAnswer(String eval) {
+        System.out.format("Question: %s%n", eval);
+        System.out.print("Your answer: ");
+        return new Scanner(System.in).nextLine();
+    }
+
+    protected String itsTrue(String name) {
+        return String.format("Congratulations, %s!", name);
+    }
+
+    protected String incorrect(String answer, String check, String name) {
+        return new StringBuilder("'")
+                .append(answer)
+                .append("' is wrong answer ;(. Correct answer was '")
+                .append(check)
+                .append("'.\nLet's try again, ")
+                .append(name)
+                .append("!")
+                .toString();
+    }
+}
