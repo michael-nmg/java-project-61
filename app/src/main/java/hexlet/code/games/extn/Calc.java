debe90da07f623a1eb2fa5ac7df35a4b190172fa
michael-nmg <michael.nmg@gmail.com>
2023-07-21 05:14:14 +0300

    class Calc & Even extends PatternForGame

---
+++ app/src/main/java/hexlet/code/games/extn/Calc.java
@@ -0,0 +1,37 @@
+package hexlet.code.games.impl;
+
+import hexlet.code.games.PatternForGame;
+
+public class Calc extends PatternForGame {
+
+    private final String[] operations = {"+", "-", "*"};
+
+    @Override
+    protected String[] invariant() {
+        int left = random.nextInt(100);
+        int right = random.nextInt(100);
+        String oper = operations[random.nextInt(3)];
+        String eval = String.format("%s %s %s", left, oper, right);
+        String check = String.valueOf(doCorrect(left, right, oper));
+        String answer = doAnswer(eval);
+        return new String[]{answer, check};
+    }
+
+    @Override
+    protected void greeting() {
+        System.out.println("\nWhat is the result of the expression?");
+    }
+
+    private int doCorrect(int left, int right, String oper) {
+        return switch (oper) {
+            case "+" -> left + right;
+            case "-" -> left - right;
+            default -> left * right;
+        };
+    }
+
+    public String getName() {
+        return "Calc";
+    }
+
+}
---
+++ app/src/main/java/hexlet/code/games/extn/Even.java
@@ -0,0 +1,24 @@
+package hexlet.code.games.impl;
+
+import hexlet.code.games.PatternForGame;
+
+public class Even extends PatternForGame {
+
+    @Override
+    protected String[] invariant() {
+        int number = random.nextInt(1000);
+        String check = (number % 2 == 0) ? "yes" : "no";
+        String answer = doAnswer(String.valueOf(number));
+        return new String[]{answer, check};
+    }
+
+    @Override
+    protected void greeting() {
+        System.out.println("\nAnswer 'yes' if the number is even, otherwise answer 'no'.");
+    }
+
+    public String getName() {
+        return "Even";
+    }
+
+}
