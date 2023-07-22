862d695eda2b71669d2060cc7ec8ab394047b4d8
michael-nmg <michael.nmg@gmail.com>
2023-07-22 20:27:00 +0300

    fix Prime class

--- app/src/main/java/hexlet/code/games/extn/Prime.java
+++ app/src/main/java/hexlet/code/games/extn/Prime.java
@@ -1,14 +1,17 @@
 package hexlet.code.games.extn;
 
 import static hexlet.code.Utils.isPrime;
+import static hexlet.code.Utils.getRandomNumber;
 
 import hexlet.code.games.PatternForGame;
 
-public class Prime extends PatternForGame {
+public final class Prime extends PatternForGame {
+
+    private final int endOfRange = 3573;
 
     @Override
     protected String[] invariant() {
-        int number = random.nextInt(3573);
+        int number = getRandomNumber(endOfRange);
         String check = isPrime(number) ? "yes" : "no";
         String answer = doAnswer(String.valueOf(number));
         return new String[]{answer, check};
