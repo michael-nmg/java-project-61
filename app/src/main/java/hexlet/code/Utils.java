d89a4218f493348310139e322cf46ccbfce2cfa6
michael-nmg <michael.nmg@gmail.com>
2023-07-23 01:21:17 +0300

    fix Utils class

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -5,8 +5,6 @@ import java.util.Random;
 
 public class Utils {
 
-    private static int lengthSequence = 10;
-
     private static final int[] PRIMELIST = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
         71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181,
         191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311,
@@ -44,15 +42,15 @@ public class Utils {
         return false;
     }
 
-    public static String makeSequence(int number, int diff) {
+    public static String makeSequence(int size, int holeNumber, int diff) {
         StringBuilder result = new StringBuilder();
 
-        for (int i = 0; i < lengthSequence; ++i) {
-            int elem = (i + 1) * diff;
-            String str = String.format("%s", i == number ? ".." : elem);
+        for (int i = 1; i <= size; ++i) {
+            int elem = i * diff;
+            String str = String.format("%s", i == holeNumber + 1 ? ".." : elem);
             result.append(str);
 
-            if (i != lengthSequence - 1) {
+            if (i != size) {
                 result.append(" ");
             }
         }
