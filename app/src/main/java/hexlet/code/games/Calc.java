acc0991e2c99ef8370dfb5ff0cb4241c3bc59e31
michael-nmg <michael.nmg@gmail.com>
2023-07-24 06:44:33 +0300

    fix Calc class: fix MagicNumber

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -7,8 +7,8 @@ import static hexlet.code.Utils.getRandomNumber;
 
 public class Calc {
 
-    private static int endOfRange = 100;
-    private static int countOperations = 3;
+    private static final int END_OF_RANGE = 100;
+    private static final int COUNT_OPERATIONS = 3;
     private static String[] operations = {"+", "-", "*"};
 
     public static String game(String name, int roundsCount) {
@@ -30,9 +30,9 @@ public class Calc {
     }
 
     private static String[] invariant() {
-        int left = getRandomNumber(endOfRange);
-        int right = getRandomNumber(endOfRange);
-        String oper = operations[getRandomNumber(countOperations)];
+        int left = getRandomNumber(END_OF_RANGE);
+        int right = getRandomNumber(END_OF_RANGE);
+        String oper = operations[getRandomNumber(COUNT_OPERATIONS)];
         String question = String.format("%s %s %s", left, oper, right);
         String check = String.valueOf(calcEval(left, right, oper));
         return new String[]{question, check};
