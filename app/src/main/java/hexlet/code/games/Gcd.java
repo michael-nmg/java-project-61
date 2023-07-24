bfb7e6490cbb1155007da52e69b68cbbbcd1d60b
michael-nmg <michael.nmg@gmail.com>
2023-07-24 06:45:00 +0300

    fix Gcd class: fix MagicNumber

--- app/src/main/java/hexlet/code/games/Gcd.java
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -7,7 +7,7 @@ import static hexlet.code.Utils.getRandomNumber;
 
 public final class Gcd {
 
-    private static int endOfRange = 100;
+    private static final int END_OF_RANGE = 100;
 
     public static String game(String name, int roundsCount) {
         description();
@@ -28,8 +28,8 @@ public final class Gcd {
     }
 
     private static String[] invariant() {
-        int left = getRandomNumber(endOfRange);
-        int rigth = getRandomNumber(endOfRange);
+        int left = getRandomNumber(END_OF_RANGE);
+        int rigth = getRandomNumber(END_OF_RANGE);
         rigth += (rigth == 0) ? 1 : 0;
         String check = String.valueOf(gcd(left, rigth));
         String question = String.format("%s %s", left, rigth);
