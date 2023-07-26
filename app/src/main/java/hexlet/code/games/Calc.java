a2ea7c59af96d82aa12313c7f23410ab0f882e72
michael-nmg <michael.nmg@gmail.com>
2023-07-26 18:29:39 +0300

    fix Calc class

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -1,7 +1,5 @@
 package hexlet.code.games;
 
-import static hexlet.code.Response.round;
-import static hexlet.code.Response.itsTrue;
 import static hexlet.code.Utils.calcEval;
 import static hexlet.code.Utils.getRandomNumber;
 
@@ -11,25 +9,7 @@ public class Calc {
     private static final int COUNT_OPERATIONS = 3;
     private static String[] operations = {"+", "-", "*"};
 
-    public static String game(String name, int roundsCount) {
-        description();
-        int count = 0;
-
-        while (count < roundsCount) {
-            String result = round(name, invariant());
-
-            if (!result.equals("Correct!")) {
-                return result;
-            }
-
-            System.out.println("Correct!");
-            ++count;
-        }
-
-        return itsTrue(name);
-    }
-
-    private static String[] invariant() {
+    public static String[] invariant() {
         int left = getRandomNumber(END_OF_RANGE);
         int right = getRandomNumber(END_OF_RANGE);
         String oper = operations[getRandomNumber(COUNT_OPERATIONS)];
@@ -38,8 +18,8 @@ public class Calc {
         return new String[]{question, check};
     }
 
-    private static void description() {
-        System.out.println("\nWhat is the result of the expression?");
+    public static void description() {
+        System.out.println("What is the result of the expression?");
     }
 
 }
