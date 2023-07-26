c2b2e9ecef16a7c5f698a9789d22554b4ae3d818
michael-nmg <michael.nmg@gmail.com>
2023-07-26 18:30:04 +0300

    fix Gcd class

--- app/src/main/java/hexlet/code/games/Gcd.java
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -1,7 +1,5 @@
 package hexlet.code.games;
 
-import static hexlet.code.Response.itsTrue;
-import static hexlet.code.Response.round;
 import static hexlet.code.Utils.gcd;
 import static hexlet.code.Utils.getRandomNumber;
 
@@ -9,25 +7,7 @@ public final class Gcd {
 
     private static final int END_OF_RANGE = 100;
 
-    public static String game(String name, int roundsCount) {
-        description();
-        int count = 0;
-
-        while (count < roundsCount) {
-            String result = round(name, invariant());
-
-            if (!result.equals("Correct!")) {
-                return result;
-            }
-
-            System.out.println("Correct!");
-            ++count;
-        }
-
-        return itsTrue(name);
-    }
-
-    private static String[] invariant() {
+    public static String[] invariant() {
         int left = getRandomNumber(END_OF_RANGE);
         int rigth = getRandomNumber(END_OF_RANGE);
         rigth += (rigth == 0) ? 1 : 0;
@@ -36,8 +16,8 @@ public final class Gcd {
         return new String[]{question, check};
     }
 
-    private static void description() {
-        System.out.println("\nFind the greatest common divisor of given numbers.");
+    public static void description() {
+        System.out.println("Find the greatest common divisor of given numbers.");
     }
 
 }
