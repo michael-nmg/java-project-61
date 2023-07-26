39745107db17fe17dde7c0b2af24649175dda409
michael-nmg <michael.nmg@gmail.com>
2023-07-26 18:29:52 +0300

    fix Even class

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,39 +1,19 @@
 package hexlet.code.games;
 
-import static hexlet.code.Response.round;
-import static hexlet.code.Response.itsTrue;
 import static hexlet.code.Utils.getRandomNumber;
 
 public final class Even {
 
     private static final int END_OF_RANGE = 1000;
 
-    public static String game(String name, int roundsCount) {
-        description();
-        int count = 0;
-
-        while (count < roundsCount) {
-            String result = round(name, invariant());
-
-            if (!result.equals("Correct!")) {
-                return result;
-            }
-
-            System.out.println("Correct!");
-            ++count;
-        }
-
-        return itsTrue(name);
-    }
-
-    private static String[] invariant() {
+    public static String[] invariant() {
         int number = getRandomNumber(END_OF_RANGE);
         String check = (number % 2 == 0) ? "yes" : "no";
         return new String[]{String.valueOf(number), check};
     }
 
-    private static void description() {
-        System.out.println("\nAnswer 'yes' if the number is even, otherwise answer 'no'.");
+    public static void description() {
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
     }
 
 }
