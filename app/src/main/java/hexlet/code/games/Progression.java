86e7edf8fd7568bd58ab1cbb5fe31450090d0c79
michael-nmg <michael.nmg@gmail.com>
2023-07-26 18:30:32 +0300

    fix Progression class

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,7 +1,5 @@
 package hexlet.code.games;
 
-import static hexlet.code.Response.round;
-import static hexlet.code.Response.itsTrue;
 import static hexlet.code.Utils.makeSequence;
 import static hexlet.code.Utils.getRandomNumber;
 
@@ -10,25 +8,7 @@ public final class Progression {
     private static final int LENGTH_SEQUENCE = 10;
     private static final int RANGE_OF_DIFFERENCE = 100;
 
-    public static String game(String name, int roundsCount) {
-        description();
-        int count = 0;
-
-        while (count < roundsCount) {
-            String result = round(name, invariant());
-
-            if (!result.equals("Correct!")) {
-                return result;
-            }
-
-            System.out.println("Correct!");
-            ++count;
-        }
-
-        return itsTrue(name);
-    }
-
-    private static String[] invariant() {
+    public static String[] invariant() {
         int diff = getRandomNumber(RANGE_OF_DIFFERENCE);
         int holeNumber = getRandomNumber(LENGTH_SEQUENCE);
         String check = String.valueOf((holeNumber + 1) * diff);
@@ -36,8 +16,8 @@ public final class Progression {
         return new String[]{question, check};
     }
 
-    private static void description() {
-        System.out.println("\nWhat number is missing in the progression?");
+    public static void description() {
+        System.out.println("What number is missing in the progression?");
     }
 
 }
