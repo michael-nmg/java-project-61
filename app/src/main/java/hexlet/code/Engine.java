428f9e2f1e9affd11238682e0870e02245af0bd5
michael-nmg <michael.nmg@gmail.com>
2023-07-26 18:29:03 +0300

    fix Engine class

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -11,42 +11,46 @@ import java.util.Scanner;
 public class Engine {
 
     private static final int GAME_ROUNDS = 3;
-    private static final int EXIT_TITLE = 0;
-    private static final int GREETING_TITLE = 1;
     private static final int EVEN_TITLE = 2;
     private static final int CALC_TITLE = 3;
     private static final int GCD_TITLE = 4;
     private static final int PROGRESSION_TITLE = 5;
-    private static String[] games = {"Even", "Calc", "Gcd", "Progression", "Prime"};
+    private static final String[] GAMES = {"Even", "Calc", "Gcd", "Progression", "Prime"};
 
     public static void start() {
         while (true) {
-            System.out.println(previewTitles());
+            previewTitles();
             Integer choice = doSelectTitle();
 
             if (choice == null) {
-                System.out.println("\nTry again.\n");
+                RoundUtils.nextTry();
                 continue;
+            } else if (choice == 0) {
+                RoundUtils.goodbye();
+                break;
+            } else if (choice == 1) {
+                Cli.greeting();
+                break;
             }
 
-            String result = selectChoice(choice);
+            String result = roundsControl(choice);
             System.out.println(result);
             break;
         }
     }
 
-    public static String previewTitles() {
+    public static void previewTitles() {
         int index = 2;
         StringBuilder list = new StringBuilder();
         list.append("Please enter the game number and press Enter.\n");
         list.append("1 - Greet\n");
 
-        for (String title : games) {
+        for (String title : GAMES) {
             list.append(String.format("%s - %s%n", index++, title));
         }
 
         list.append("0 - Exit");
-        return list.toString();
+        System.out.println(list);
     }
 
     private static Integer doSelectTitle() {
@@ -59,28 +63,50 @@ public class Engine {
 
         int choice = scan.nextInt();
 
-        if (choice < 0 || games.length + 1 < choice) {
+        if (choice < 0 || GAMES.length + 1 < choice) {
             return null;
         }
 
         return choice;
     }
 
-    private static String voidStringAfterGreeting() {
-        Cli.greeting();
-        return "";
+    private static String roundsControl(int choice) {
+        int count = 0;
+        String name = Cli.greeting();
+        getDescription(choice);
+
+        while (count < GAME_ROUNDS) {
+            String result = RoundUtils.getRound(name, getGameInvariant(choice));
+
+            if (!result.equals("Correct!")) {
+                return result;
+            }
+
+            System.out.println(result);
+            ++count;
+        }
+
+        return RoundUtils.itsTrue(name);
     }
 
-    public static String selectChoice(int choice) {
+    private static String[] getGameInvariant(int choice) {
         return switch (choice) {
-            case EXIT_TITLE -> "Goodbye.";
-            case GREETING_TITLE -> voidStringAfterGreeting();
-            case EVEN_TITLE -> Even.game(Cli.greeting(), GAME_ROUNDS);
-            case CALC_TITLE -> Calc.game(Cli.greeting(), GAME_ROUNDS);
-            case GCD_TITLE -> Gcd.game(Cli.greeting(), GAME_ROUNDS);
-            case PROGRESSION_TITLE -> Progression.game(Cli.greeting(), GAME_ROUNDS);
-            default -> Prime.game(Cli.greeting(), GAME_ROUNDS);
+            case EVEN_TITLE -> Even.invariant();
+            case CALC_TITLE -> Calc.invariant();
+            case GCD_TITLE -> Gcd.invariant();
+            case PROGRESSION_TITLE -> Progression.invariant();
+            default -> Prime.invariant();
         };
+    }
+
+    private static void getDescription(int choice) {
+        switch (choice) {
+            case EVEN_TITLE -> Even.description();
+            case CALC_TITLE -> Calc.description();
+            case GCD_TITLE -> Gcd.description();
+            case PROGRESSION_TITLE -> Progression.description();
+            default -> Prime.description();
+        }
     }
 
 }
